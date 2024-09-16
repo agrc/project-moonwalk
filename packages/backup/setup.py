9513c80b4d3c944beba5944e6f0c97b37e5045e4
Jake Adams <jdadams@utah.gov>
2024-09-16 13:22:11 -0600

    chore: initial setup pt 2
    Co-authored-by: Scott Davis <stdavis@utah.gov>

---
+++ packages/backup/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+setup.py
+A module that installs the backup process as a module
+"""
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages, setup
+
+setup(
+    name="moonwalk-backup",
+    version="1.0.0",
+    license="MIT",
+    description="Backup process for project moonwalk",
+    author="UGRC Developers",
+    author_email="ugrc-developers@utah.gov",
+    url="https://github.com/agrc/project-moonwalk",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+    ],
+    project_urls={
+        "Issue Tracker": "https://github.com/agrc/project-moonwalk/issues",
+    },
+    keywords=["gis"],
+    install_requires=[
+        "arcgis>=2.3,<2.4",
+    ],
+    extras_require={
+        "tests": [
+            "pytest-cov>=3,<6",
+            "pytest-instafail==0.5.*",
+            "pytest-mock==3.*",
+            "pytest-ruff==0.*",
+            "pytest-watch==4.*",
+            "pytest>=6,<9",
+            "black>=24.4.2,<24.5",
+            "ruff==0.*",
+        ]
+    },
+    setup_requires=[
+        "pytest-runner",
+    ],
+    entry_points={
+        "console_scripts": [
+            "backup = backup.main:backup",
+        ]
+    },
+)
