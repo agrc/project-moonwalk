82a8beb80f286d846bb08674465c286fde662cea
Jake Adams <jdadams@utah.gov>
2024-09-16 13:22:11 -0600

    chore: initial setup pt 2
    Co-authored-by: Scott Davis <stdavis@utah.gov>

---
+++ packages/backup/src/backup/main.py
@@ -0,0 +1,37 @@
+import json
+from pathlib import Path
+
+import arcgis
+
+
+def _get_secrets():
+    """A helper method for loading secrets from either a GCF mount point or the local src/nfhl-skid/secrets/secrets.
+    json file
+
+    Raises:
+        FileNotFoundError: If the secrets file can't be found.
+
+    Returns:
+        dict: The secrets .json loaded as a dictionary
+    """
+
+    secret_folder = Path("/secrets")
+
+    #: Try to get the secrets from the Cloud Function mount point
+    if secret_folder.exists():
+        return json.loads(Path("/secrets/app/secrets.json").read_text(encoding="utf-8"))
+
+    #: Otherwise, try to load a local copy for local development
+    secret_folder = Path(__file__).parent / "secrets"
+    if secret_folder.exists():
+        return json.loads((secret_folder / "secrets.json").read_text(encoding="utf-8"))
+
+    raise FileNotFoundError("Secrets folder not found; secrets not loaded.")
+
+
+def backup():
+    pass
+
+
+if __name__ == "__main__":
+    backup()
